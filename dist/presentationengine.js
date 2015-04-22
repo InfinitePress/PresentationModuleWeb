bf871cbc88bebd014c6e33d0c132699b2f23efb3
Clovis Chedid <coichedid@gmail.com>
2015-04-22 19:54:38 -0300

    Initial module creation

---
+++ dist/presentationengine.js
@@ -0,0 +1,35 @@
+(function (angular) {
+
+  // Create all modules and define dependencies to make sure they exist
+  // and are loaded in the correct order to satisfy dependency injection
+  // before all nested files are concatenated by Gulp
+
+  // Config
+  angular.module('presentationengine.config', [])
+      .value('presentationengine.config', {
+          debug: true
+      });
+
+  // Modules
+  
+  angular.module('presentationengine.directives', []);
+  
+  
+  angular.module('presentationengine.filters', []);
+  
+  
+  angular.module('presentationengine.services', []);
+  
+  
+    angular.module('presentationengine.controllers', []);
+  
+  angular.module('presentationengine',
+      [
+        'presentationengine.config',
+        'presentationengine.directives',
+        'presentationengine.filters',
+        'presentationengine.services',
+        'presentationengine.controllers'
+      ]);
+
+})(angular);
