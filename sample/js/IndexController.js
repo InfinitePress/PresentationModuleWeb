870ee2c37bb42ac8c8a4a8ba6332d536c71a297b
Clovis Chedid <coichedid@gmail.com>
2015-04-22 20:38:22 -0300

    sample index with angular

--- sample/js/IndexController.js
+++ sample/js/IndexController.js
@@ -1,3 +1,13 @@
 /**
  * Created by coichedid on 22/04/15.
  */
+app.controller('presentationController', function($scope){
+    $scope.welcome = 'InfinitePress Presentation Sample';
+    try {
+        angular.module("presentationengine");
+        $scope.presentationengineExists = true;
+    } catch(err) {
+        $scope.presentationengineExists = false;
+    /* failed to require */
+    }
+});
