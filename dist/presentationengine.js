79c015653f827dc5004dfe5c94671f6885e30f6e
Clovis Chedid <coichedid@gmail.com>
2015-04-23 18:46:02 -0300

    sample with presentation-screen and presentation-step

--- dist/presentationengine.js
+++ dist/presentationengine.js
@@ -33,3 +33,81 @@
       ]);
 
 })(angular);
+
+/**
+ * Created by coichedid on 21/04/15.
+ */
+'use strict';
+angular.module('presentationengine').directive('presentationScreen', [
+  function() {
+    return {
+      restrict: 'E',
+      replace: 'false',
+      scope: {},
+      templateUrl: 'view-templates/presentation-screen-view.html',
+      link: function postLink(scope, element, attrs) {
+
+      }
+    };
+  }
+]);
+
+/**
+ * Created by coichedid on 21/04/15.
+ */
+'use strict';
+angular.module('presentationengine').directive('presentationShapeImage', [
+  function() {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      replace: 'false',
+      scope: {},
+      link: function postLink(scope, element, attrs) {
+        // Presentation shape image directive logic
+        // ...
+
+        element.text('this is the presentationShapeImage directive');
+      }
+    };
+  }
+]);
+
+/**
+ * Created by coichedid on 21/04/15.
+ */
+'use strict';
+angular.module('presentationengine').directive('presentationShapeText', [
+  function() {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      replace: 'false',
+      scope: {},
+      link: function postLink(scope, element, attrs) {
+        // Presentation shape text directive logic
+        // ...
+
+        element.text('this is the presentationShapeText directive');
+      }
+    };
+  }
+]);
+
+/**
+ * Created by coichedid on 21/04/15.
+ */
+'use strict';
+angular.module('presentationengine').directive('presentationStep', [
+  function() {
+    return {
+      restrict: 'E',
+      replace: 'false',
+      scope: {},
+      templateUrl: 'view-templates/presentation-step-view.html',
+      link: function postLink(scope, element, attrs) {
+
+      }
+    };
+  }
+]);
