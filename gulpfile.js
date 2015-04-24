5e824e54054a1390b04f4f40bca6935e148d4c8f
Clovis Chedid <coichedid@gmail.com>
2015-04-23 22:40:15 -0300

    set templateCache to correct angular module

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var templateCache = require('gulp-angular-templatecache');
 var clean = require('gulp-clean');
 var debug = require('gulp-debug');
 var streamQueue = require('streamqueue');
+var fs = require('fs');
 
 /**
  * File patterns
@@ -25,73 +26,51 @@ var sourceDirectory = path.join(rootDirectory, './modules');
 
 var sourceFiles = [
 
-  // Make sure module files are handled first
-  path.join(sourceDirectory, '/**/*.module.js'),
-  // Then add all JavaScript files
-  path.join(sourceDirectory, '/**/*.js')
+  //// Make sure module files are handled first
+  //path.join(sourceDirectory, '/**/*.module.js'),
+  //// Then add all JavaScript files
+  //path.join(sourceDirectory, '/**/*.js')
 ];
 
-var buildFiles = [
-
-  // Make sure module files are handled first
-  path.join(sourceDirectory, '/**/*.module.js'),
-  // Then add templates to cache
-  path.join(rootDirectory,'/dist/templates*.js'),
-  // Then add all JavaScript files
-  path.join(sourceDirectory, '/**/*.js')
-];
 var resourceFiles = [
-  path.join(sourceDirectory,'/**/*.html')
+  //path.join(sourceDirectory,'/**/*.html')
 ];
-var resourceStream = {};
-console.log(resourceFiles);
+
+if (fs.existsSync(sourceDirectory)) {
+  fs.readdirSync(sourceDirectory).forEach(function(folder) {
+    var stat = fs.statSync(sourceDirectory + '/' + folder);
+    if (stat.isDirectory()) {
+      var module = {
+        name:folder,
+        sourceFiles: [
+          path.join(sourceDirectory, '/' + folder + '/*.module.js'),
+          path.join(sourceDirectory, '/' + folder + '/**/*.js')
+        ],
+        resourceFiles: [
+          path.join(sourceDirectory, '/' + folder + '/directives/view-templates/*.html')
+        ],
+        resourceStream: {}
+      };
+      sourceFiles.push(module);
+    }
+  });
+}
 
 var lintFiles = [
+  // Make sure module files are handled first
+  path.join(sourceDirectory, '/**/*.module.js'),
+  // Then add all JavaScript files
+  path.join(sourceDirectory, '/**/*.js'),
   'gulpfile.js',
   // Karma configuration
   'karma-*.conf.js'
-].concat(sourceFiles);
+];
+  //.concat(sourceFiles);
 
 gulp.task('clean_build', function() {
   var distFolder = [path.join(rootDirectory,'/dist')];
   gulp.src(distFolder)
     .pipe(clean());
-});
-
-gulp.task('build', function() {
-  console.log(buildFiles);
-  var buildStream = gulp.src(sourceFiles);
-  var stream = streamQueue({ objectMode: true }, resourceStream, buildStream);
-    stream.pipe(debug())
-    .pipe(plumber())
-    .pipe(concat('presentationengine.js'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(uglify())
-    .pipe(rename('presentationengine.min.js'))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('build_resources', function() {
-  resourceStream = gulp.src(resourceFiles)
-    .pipe(flatten())
-    .pipe(templateCache('templates.js',{root:'view-templates',standalone:true}))
-    .pipe(gulp.dest('./dist'));
-});
-
-/**
- * Process
- */
-gulp.task('process-all', function (done) {
-  runSequence('clean_build', 'jshint', 'test-src', 'build_resources', 'build',  done);
-});
-
-/**
- * Watch task
- */
-gulp.task('watch', function () {
-
-  // Watch JavaScript files
-  gulp.watch(sourceFiles, ['process-all']);
 });
 
 /**
@@ -133,6 +112,61 @@ gulp.task('test-dist-minified', function (done) {
     configFile: __dirname + '/karma-dist-minified.conf.js',
     singleRun: true
   }, done);
+});
+
+gulp.task('build', function() {
+  sourceFiles.forEach(function(module){
+    var moduleStream = gulp.src(module.sourceFiles[0]),
+      sourceStream = gulp.src(module.sourceFiles[1]),
+      stream = streamQueue({ objectMode: true }, moduleStream, module.resourceStream, sourceStream);
+
+    stream.pipe(debug())
+      .pipe(plumber())
+      .pipe(concat('presentationengine.js'))
+      .pipe(gulp.dest('./dist/'))
+      .pipe(uglify())
+      .pipe(rename('presentationengine.min.js'))
+      .pipe(gulp.dest('./dist'));
+  });
+  //var buildStream = gulp.src(sourceFiles);
+  //var stream = streamQueue({ objectMode: true }, resourceStream, buildStream);
+  //  stream.pipe(debug())
+  //  .pipe(plumber())
+  //  .pipe(concat('presentationengine.js'))
+  //  .pipe(gulp.dest('./dist/'))
+  //  .pipe(uglify())
+  //  .pipe(rename('presentationengine.min.js'))
+  //  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build_resources', function() {
+  sourceFiles.forEach(function(module){
+    var resourceStream = gulp.src(module.resourceFiles[0])
+      .pipe(flatten())
+      .pipe(templateCache(module.name + '-templates.js',{root:'view-templates',module:module.name}))
+      .pipe(gulp.dest('./dist'));
+    module.resourceStream = resourceStream;
+  });
+  //resourceStream = gulp.src(resourceFiles)
+  //  .pipe(flatten())
+  //  .pipe(templateCache('templates.js',{root:'view-templates',standalone:true}))
+  //  .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * Process
+ */
+gulp.task('process-all', function (done) {
+  runSequence('clean_build', 'jshint', 'test-src', 'build_resources', 'build',  done);
+});
+
+/**
+ * Watch task
+ */
+gulp.task('watch', function () {
+
+  // Watch JavaScript files
+  gulp.watch(sourceFiles, ['process-all']);
 });
 
 gulp.task('default', function () {
