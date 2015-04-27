1437354280f395be89745771facb46da6ae7ab9c
Clovis Chedid <coichedid@gmail.com>
2015-04-27 19:07:06 -0300

    samples of presentation screen and presentation step directives

--- sample/js/IndexController.js
+++ sample/js/IndexController.js
@@ -10,4 +10,16 @@ app.controller('presentationController', function($scope){
         $scope.presentationengineExists = false;
     /* failed to require */
     }
+
+    $scope.presentationData = {
+      steps:[
+        {elements:[
+          {content:'Element 1 - Content 1'},
+          {content:'Element 1 - Content 2'}
+        ]},
+        {elements:[
+          {content:'Element 2 - Content 1'}
+        ]}
+      ]
+    };
 });
